474a8cd56bd0cc6f2fcce0d4da470cc43f9b7cbc
mlmesa <mlmesa.dev@gmail.com>
2026-01-23 17:50:29 -0600

    chore: Update application versioning
    - Increment `versionCode` to 5.
    - Update `versionName` to "0.0.7".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.mlmesa.savingdays"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.0.6"
+        versionCode = 5
+        versionName = "0.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
